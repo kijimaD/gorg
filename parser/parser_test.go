304cc3862e8191a3345dc3574a102f8d3323a9b4
kijimaD <norimaking777@gmail.com>
2022-10-18 22:50:03 +0900

    test: assert nodes

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -1,11 +1,13 @@
 package parser
 
 import (
+	"gorg/ast"
 	"gorg/lexer"
+	"strings"
 	"testing"
 )
 
-func TestHeaderStatements(t *testing.T) {
+func TestHeaderNodes(t *testing.T) {
 	input := `
 * header1
 ** header2
@@ -17,5 +19,17 @@ func TestHeaderStatements(t *testing.T) {
 	if len(org.Nodes) != 2 {
 		t.Fatalf("program has not enough nodes. got=%d",
 			len(org.Nodes))
+	}
+
+	for _, node := range org.Nodes {
+		headerNode, ok := node.(*ast.Header)
+		if !ok {
+			t.Errorf("headerNode not *ast.Header. got=%T", node)
+			continue
+		}
+		if headerNode.TokenLiteral() != strings.Repeat("*", headerNode.Level) {
+			t.Errorf("headerNode.TokenLiteral not 'header'. got %q",
+				node.TokenLiteral())
+		}
 	}
 }
