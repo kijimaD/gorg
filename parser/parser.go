e13e8a308064c621a0a0c2464af650ee4d6c01a6
kijimaD <norimaking777@gmail.com>
2022-10-20 10:02:40 +0900

    refactor: common infix function

--- parser/parser.go
+++ parser/parser.go
@@ -5,12 +5,14 @@ import (
 	"bytes"
 	"fmt"
 	"gorg/ast"
+	"gorg/token"
 	"regexp"
 )
 
 const (
 	HEADER1_REGEXP = `^\* (.*)`
 	HEADER2_REGEXP = `^\*\* (.*)`
+	COMMENT_REGEXP = `^# (.*)`
 )
 
 type Parser struct {
@@ -63,31 +65,10 @@ func (p *Parser) parseNode(o *ast.Org, str string, parent ast.Node) {
 
 		normal := &ast.Normal{Value: p.parseComment(str), Parent: comment}
 		o.Nodes = append(o.Nodes, normal)
-	} else if len(p.parseTag(str, "*")) > 0 {
+	} else if len(p.matchInfixTag(str, token.ASTERISK)) > 0 {
 		// bold
-		matches := p.parseTag(str, "*")
+		left, center, right := p.parseInfixTag(str, token.ASTERISK)
 
-		var left string
-		var center string
-		var right string
-		if len(matches) == 4 {
-			left = matches[1]
-			center = matches[2]
-			right = matches[3]
-		} else if len(matches) == 2 {
-			left = ""
-			center = matches[1]
-			right = ""
-		} else if len(matches) == 3 && p.matchLeftTag(str, "*") && !p.matchRightTag(str, "*") {
-			left = matches[1]
-			center = matches[2]
-			right = ""
-		} else if len(matches) == 3 && !p.matchLeftTag(str, "*") && p.matchRightTag(str, "*") {
-			left = ""
-			center = matches[1]
-			right = matches[2]
-
-		}
 		p.parseNode(o, left, parent)
 
 		bold := &ast.Bold{Parent: parent}
@@ -117,8 +98,35 @@ func (p *Parser) parseHeader(s string, exp string) string {
 	return match
 }
 
-// 中置関数は共通化できそう?
-func (p *Parser) parseTag(s string, tag string) []string {
+func (p *Parser) parseInfixTag(s string, tag string) (string, string, string) {
+	matches := p.matchInfixTag(s, tag)
+
+	var left string
+	var center string
+	var right string
+	if len(matches) == 4 {
+		left = matches[1]
+		center = matches[2]
+		right = matches[3]
+	} else if len(matches) == 2 {
+		left = ""
+		center = matches[1]
+		right = ""
+	} else if len(matches) == 3 && p.matchLeftTag(s, tag) && !p.matchRightTag(s, tag) {
+		left = matches[1]
+		center = matches[2]
+		right = ""
+	} else if len(matches) == 3 && !p.matchLeftTag(s, tag) && p.matchRightTag(s, tag) {
+		left = ""
+		center = matches[1]
+		right = matches[2]
+
+	}
+
+	return left, center, right
+}
+
+func (p *Parser) matchInfixTag(s string, tag string) []string {
 	re := regexp.MustCompile(fmt.Sprintf(`(.*)\%s(.*)\%s(.*)`, tag, tag))
 	matchs := re.FindStringSubmatch(s)
 	return matchs
@@ -134,7 +142,7 @@ func (p *Parser) matchRightTag(s string, tag string) bool {
 }
 
 func (p *Parser) parseComment(s string) string {
-	re := regexp.MustCompile(`^# (.*)`)
+	re := regexp.MustCompile(COMMENT_REGEXP)
 	matches := re.FindStringSubmatch(s)
 	var match string
 	if len(matches) > 0 {
